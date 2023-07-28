99fd9548e2a52064ce56a5e2c9b704cc88a196b5
Gustavo dos Anjos <lalodosanjos@gmail.com>
2023-07-28 15:53:37 -0300

    Rate limiting

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
 
 import AppError from './utils/appError';
 import globalErrorHandler from './controllers/errorController';
@@ -9,8 +10,15 @@ import { ROOT, TOURS, USERS } from './routes/variables';
 
 const app = express();
 
-// Middlewares
+// Global Middlewares
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
