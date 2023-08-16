273f2604b1d963e06239ae46a88b9f452fd516cd
Gustavo dos Anjos <lalodosanjos@gmail.com>
2023-08-15 21:19:21 -0300

    Added indexes to improve read performance

--- dev-data/data/import-dev-data.ts
+++ dev-data/data/import-dev-data.ts
@@ -2,6 +2,9 @@ import fs from 'fs';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import Tour from '../../models/tourModel';
+import User from '../../models/userModel';
+import Review from '../../models/reviewModel';
+
 dotenv.config({ path: './config.env' });
 
 // Connect to DB
@@ -14,11 +17,17 @@ mongoose.connect(DB).then((_con) => console.log('DB connection successful!'));
 // Read JSON file
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // Import data into DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -30,6 +39,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
