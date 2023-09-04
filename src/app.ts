c2e3223012583a166b6d1cee93f34405163cc997
Gustavo dos Anjos <lalodosanjos@gmail.com>
2023-09-04 17:29:52 -0300

    Created xss-clean type definition file

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import morgan from 'morgan';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import mongoSanitize from 'express-mongo-sanitize';
-// @ts-ignore
 import xss from 'xss-clean';
 import hpp from 'hpp';
 import cookieParser from 'cookie-parser';
